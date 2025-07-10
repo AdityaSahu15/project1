ab47aed335e37a3443b236615f37ab5126559136
AdityaSahu15 <itsadityasahu1509@gmail.com>
2025-07-11 03:15:07 +0530

    fixed cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,9 +13,11 @@ import {getWishlist, addToWishlist, removeFromWishlist,getWishlistItems} from ".
 const app = express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json())
 
